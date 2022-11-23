80c010a02872ef5cd66219c8ca77bc7dcae9cd68
nmcamilis <nmcamilistrab@gmail.com>
2022-11-22 21:27:50 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _17_11_atividade.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
